2a8f929ba550ba8168492116e36a127151c59457
hvacrua <kras_pp@ukr.net>
2021-08-19 13:42:20 +0300

    point2 - done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Execute test task step-by-step:
 1. Design Action Pipeline at Git.
-2. In process.
+2. Done: https://4czxqqyjya.execute-api.us-east-2.amazonaws.com/view
 3. In process.
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-msg = "Hello World!"
+def greeting():
+    """Return greeting"""
+    msg = 'Hello World!'
+    
+    return msg
 
-print(msg)
+
+if __name__ == '__main__':
+    greeting()
